eee9f57dbaeda29f1ede8d1dc2d697d3f4923b3a
MuthuRaj5643 <muthuraj5643@gmail.com>
2021-12-21 22:46:20 +0530

    big query schema modularized

--- modules/app_engine_existing_project/main.tf
+++ modules/app_engine_existing_project/main.tf
@@ -10,6 +10,11 @@ resource "google_project_service" "app_engine_service" {
     disable_dependent_services = true
 }
 
+resource "google_project_service" "cloud_resource_service" {
+    project = var.project_id
+    service = "cloudresourcemanager.googleapis.com"
+    disable_dependent_services = true
+}
 resource "google_app_engine_application" "app" {
 
     project = var.project_id
--- modules/big_query/main.tf
+++ modules/big_query/main.tf
@@ -1,40 +1,17 @@
 resource "google_bigquery_dataset" "sample" {
-        project = var.project_id
-        dataset_id = var.dataset_id
-        friendly_name = var.friendly_name
-        description = var.description
-        location = var.location      
+    project = var.project_id
+    dataset_id = var.dataset_id
+    friendly_name = var.friendly_name
+    description = var.description
+    location = var.location      
 }
 
 resource "google_bigquery_table" "default" {
-  project = var.project_id
-  dataset_id = google_bigquery_dataset.sample.dataset_id
-  table_id   = var.table_id
-
-
-  schema = <<EOF
-[
-  {
-    "name": "firstname",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "first name"
-  },
-  {
-    "name": "lastname",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "last name"
-  },
-  {
-    "name": "salary" ,
-    "type": "INTEGER" ,
-    "mode": "NULLABLE" ,
-    "description": "salary of an individual"
-  }
-]
-EOF
-
+    project = var.project_id
+    dataset_id = google_bigquery_dataset.sample.dataset_id
+    table_id   = var.table_id
+    schema = file("../modules/big_query/table_schema.json")
+        
     external_data_configuration{
         autodetect = var.autodetect
         source_format = var.source_format
